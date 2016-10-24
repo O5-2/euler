4a1d28cd59cce00fc9899afbb81130058ff0ae99
Elijah Karp <eli@Elijahs-MacBook-Air.local>
2016-10-23 19:47:05 -0700

    Made problem 3 code faster

--- problem3.go
+++ problem3.go
@@ -5,8 +5,16 @@ import (
 	"math"
 )
 
+func max(x, y int) int {
+	if x < y {
+		return y
+	} else {
+		return x
+	}
+}
+
 func is_prime(y int) bool {
-	max := (y / 2) + 1
+	max := int(math.Sqrt(float64(y))) + 1
 	for i := 2; i < max; i++ {
 		if y%i == 0 {
 			return false
@@ -15,25 +23,21 @@ func is_prime(y int) bool {
 	return true
 }
 
-func prime_factors(x int) []int {
+func max_factor(x int) int {
 	maximum := (x / 2) + 1
-	list := []int{}
-	primes := []int{}
 	for i := 2; i < maximum; i++ {
 		if is_prime(i) {
-			primes = append(primes, i)
+			if x%i == 0 {
+				compare := max_factor(x / i)
+				return max(compare, i)
+			}
 		}
 	}
-	for i := 0; i < len(primes); i++ {
-		if x%primes[i] == 0 {
-			list = append(list, primes[i])
-		}
-	}
-	return list
+	return x
 }
 
 func main() {
-	fmt.Printf("%v", prime_factors(600851475143))
+	fmt.Printf("%v\n", max_factor(600851475143))
 }
 
 // the input is 600851475143
