e42124c2a5f1ed2dd28bb7f27562fb8ed69b5600
Elijah Karp <eli@Elijahs-MacBook-Air.local>
2017-05-18 10:58:05 -0700

    Added code for problem 19

---
+++ problem19.py
@@ -0,0 +1,23 @@
+def sundayCount(start, end):
+    months = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    date = start
+    days = 0
+    sundays = 0
+    while date != end:
+        days += 1
+        date[1] += 1
+        if date[2] % 4 == 0 and date[0] == 2:
+            if date[1] > 29:
+                date[1] = 1
+                date[0] += 1
+        elif date[1] > months[date[0]]:
+            date[1] = 1
+            date[0] += 1
+        if date[0] > 12:
+            date[0] = 1
+            date[2] += 1
+        if date[1] == 1 and days % 7 == 6:
+            sundays += 1
+    return sundays
+
+print sundayCount([12,31,1900],[12,31,2000])
