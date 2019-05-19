c1f0df9daade680b9f6451a62371b567331d17e1
Elijah Karp <elijahkarp@gmail.com>
2019-05-19 13:29:05 -0700

    cleanup and optimize

--- problem4.go
+++ problem4.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
-func is_palindrome(data int) bool {
-	stringData := strconv.Itoa(data)
-	listData := strings.Split(stringData, "")
-	for i := 0; i < 3; i++ {
-		if listData[i] != listData[len(listData)-i-1] {
-		   	return false
-		}
-	}
-	return true
+func is_palindrome_new(data int) bool {
+     var digits []int
+     for data > 0 {
+     	 last := data % 10
+     	 digits = append(digits, last)
+	 data /= 10
+     }
+     i := 0
+     j := len(digits) - 1
+     for i <= j {
+     	 if digits[i] != digits[j] {
+	    return false
+	 }
+	 i++
+	 j--
+     }
+     return true
 }
 
 func main() {
@@ -22,13 +28,10 @@ func main() {
      	 for k := 0; k < diag + 1; k++ {
 	     	x := 999-diag+k;
 		y := 999-k;
-			if is_palindrome(x * y) {
-				fmt.Printf("%v\n", x*y)
-				return
-				break
-				//break
-			}
-
+		if is_palindrome_new(x * y) {
+			fmt.Printf("%v\n", x*y)
+			return
+		}
 	 }
      }
 }
