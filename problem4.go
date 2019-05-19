1760a06535e5b8efad6e94ac5e73b1621e7cc68b
Elijah Karp <elijahkarp@gmail.com>
2019-05-19 12:59:18 -0700

    cleanup

--- problem4.go
+++ problem4.go
@@ -7,36 +7,14 @@ import (
 )
 
 func is_palindrome(data int) bool {
-	bool1 := true
 	stringData := strconv.Itoa(data)
 	listData := strings.Split(stringData, "")
 	for i := 0; i < 3; i++ {
-		if listData[0] != listData[len(listData)-1] {
-			bool1 = false
-			break
-		}
-		if len(listData) == 1 {
-			break
-		}
-		if len(listData) == 2 {
-			break
-		}
-		listData = listData[1 : len(listData)-1]
-	}
-	return bool1
-}
-
-func sol1() {
-	for i := 999; i > 99; i-- {
-		for j := 999; j >= i; j-- {
-			if is_palindrome(i * j) {
-				fmt.Printf("%v\n", i*j)
-				return
-				break
-				//break
-			}
+		if listData[i] != listData[len(listData)-i-1] {
+		   	return false
 		}
 	}
+	return true
 }
 
 func main() {
