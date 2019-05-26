f541fd5b58e44779287a827250425932361a1ca0
Elijah Karp <elijahkarp@gmail.com>
2019-05-26 15:06:37 -0700

    optimizations

--- problem12.py
+++ problem12.py
@@ -1,14 +1,28 @@
 from math import sqrt
 
+def numDivisors(num):
+    if num == 1:
+        return 1
+    divisors = 1
+    for i in range(2, int(sqrt(num))+1):
+        if num%i == 0:
+            while num%i == 0:
+                divisors += 1
+                num /= i
+            break
+    if divisors == 1:
+        return 2
+    return divisors*numDivisors(num)
+
 def triangleDivisors(atLeast):
     current = 3
     count = 3
     while True:
-        divisors = 2
         current += count
-        for i in range(2,int(sqrt(current))+1):
-            if current % i == 0:
-                divisors += 2
+        divisors = numDivisors(current) #2
+        #for i in range(2,int(sqrt(current))+1):
+        #    if current % i == 0:
+        #        divisors += 2
         if divisors >= atLeast:
             return current
         count += 1
--- problem14.py
+++ problem14.py
@@ -1,17 +1,24 @@
 lengths = {1: 1}
 
-def collatzLength(num):
+largestD = 0
+def collatzLength(num,depth):
+    global largestD
+    if depth > largestD:
+        largestD = depth
     if num not in lengths:
         if num % 2 == 0:
-            current = collatzLength(num/2)+1
+            current = collatzLength(num/2,depth+1)+1
         else:
-            current = collatzLength((num*3)+1)+1
+            current = collatzLength((num*3)+1,depth+1)+1
         lengths[num] = current
+    else:
+        x = 0
     return lengths[num]
 
 largest = 0
 for i in range(1,1000000):
-    if largest < collatzLength(i):
-        largest = collatzLength(i)
+    collatzI = collatzLength(i,1)
+    if largest < collatzI:
+        largest = collatzI
         largest_i = i
 print largest_i
